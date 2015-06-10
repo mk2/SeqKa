6b7a3979275c0d70407d80110bfe1a6cf07f563b
Harukao <mk2@mk2.red>
2015-06-11 02:10:26 +0900

    [ADD] impl reduce

--- test/seqqu.test.js
+++ test/seqqu.test.js
@@ -2,10 +2,47 @@
  *
  * Created by harukao on 2015/06/11.
  */
-var assert = require("assert"),
-    seqqu  = require("../index");
+var seqqu = require("../index");
 
-exports.test_new_seqqu = function (t) {
+exports.test_forEach = function (t) {
+
+  var k   = 1000,
+      seq = seqqu.Seq(0, function (v) { return v + k;});
+
+  console.log("Run first forEach");
+
+  seq.forEach(function (v, i) {
+
+    console.log("[forEach] index: ", i, " value: ", v);
+
+    t.equal(v, i * k);
+
+    if (i == 10) {
+      return seqqu.fin;
+    }
+  });
+
+  t.done();
+};
+
+exports.test_reduce = function (t) {
+
+  var k   = 1000,
+      seq = seqqu.Seq(0, function (v) { return v + k;});
+
+  console.log("Run first reduce");
+
+  seq.reduce(function (carried, v, i) {
+
+    console.log("[reduce] index: ", i, " carried: ", carried);
+
+    if (i < 10) {
+      return carried.concat(v);
+    }
+
+    return seqqu.fin;
+
+  }, []);
 
   t.done();
 };
